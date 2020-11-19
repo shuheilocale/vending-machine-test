5c31b6b457176507a3497730af222a0e6c3af530
shuheilocale <checkyourheadd@gmail.com>
2020-11-19 20:00:39 +0900

    Add リリース用

---
+++ build.sh
@@ -0,0 +1,4 @@
+NAME=vending-machine-test
+VERSION=0.0.1
+
+docker build -t $NAME:$VERSION .
--- debug_run.sh
+++ debug_run.sh
@@ -1,7 +1,8 @@
 NAME=vending-machine-test
 VERSION=0.0.1
 
+docker build -t $NAME:$VERSION .
 docker run -it --rm \
         -v $PWD/src:/usr/src \
         --name $NAME \
-        $NAME:$VERSION python3.9 run.py
+        $NAME:$VERSION /bin/bash
